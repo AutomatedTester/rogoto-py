66ce3b8c3b9bf7975b114f6fb59d810f60d27eee
AutomatedTester <dburns@mozilla.com>
2014-02-02 21:26:34 +0000

    Handling pendown command

--- rogoto/parser.py
+++ rogoto/parser.py
@@ -4,13 +4,17 @@ import re
 class RogotoParser(object):
     """Parse Rogoto Commands"""
     def __init__(self):
-        pass
+        self.code_to_execute = []
 
     def parse(self, commands):
         cmdRegex = r'pendown'
         matches = re.search(cmdRegex, commands)
         if matches is None:
             raise RogotoParserException('Invalid Syntax was found')
+        else:
+            self.code_to_execute.append(matches.group(0))
+
+        return self.code_to_execute
 
 
 class RogotoParserException(Exception):
